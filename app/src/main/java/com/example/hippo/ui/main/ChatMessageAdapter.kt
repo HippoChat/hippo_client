36123885c0a337eb05c8e23273a81d324425f2a9
aleksix <nope>
2020-05-13 21:14:04 +0300

    Separation of message types.

--- app/src/main/java/com/example/hippo/ui/main/ChatMessageAdapter.kt
+++ app/src/main/java/com/example/hippo/ui/main/ChatMessageAdapter.kt
@@ -2,6 +2,7 @@ package com.example.hippo.ui.main
 
 import android.content.Context
 import android.graphics.PorterDuff
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,33 +11,6 @@ import com.example.hippo.R
 import kotlinx.android.synthetic.main.chat_bubble.view.*
 
 class ChatMessageAdapter(private var context: Context, private var elements: List<Message>) : RecyclerView.Adapter<ChatMessageAdapter.ViewHolder>() {
-    /*
-    val inflater: LayoutInflater = LayoutInflater.from(context)
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View = convertView ?: inflater.inflate(R.layout.chat_bubble, parent, false)
-        //val yOffset = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20f, context.resources.displayMetrics)
-        view.message.text = elements[position].message
-        view.layoutParams.width = view.message.measuredWidth
-        //view.message.measure(0, 0)
-        //view.bubble.layoutParams.width = view.message.measuredWidth + view.message.marginRight * 2
-        //view.bubble.layoutParams.height = view.message.measuredHeight + yOffset.toInt()
-        return view
-    }
-
-    override fun getItem(position: Int): Any {
-        return elements[position]
-    }
-
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
-
-    override fun getCount(): Int {
-        return elements.size
-    }
-     */
-
     class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -51,15 +25,17 @@ class ChatMessageAdapter(private var context: Context, private var elements: Lis
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.view.message.text = elements[position].message
         if(elements[position].received) {
-            holder.view.background.setColorFilter(
+            holder.view.backLayout.background.setColorFilter(
                 context.resources.getColor(R.color.chatBubbleDark),
                 PorterDuff.Mode.SRC_IN
             )
+            holder.view.alignmentLayout.gravity = Gravity.START
         } else {
-            holder.view.background.setColorFilter(
+            holder.view.backLayout.background.setColorFilter(
                 context.resources.getColor(R.color.chatBubbleLight),
                 PorterDuff.Mode.SRC_IN
             )
+            holder.view.alignmentLayout.gravity = Gravity.END
         }
     }
 }
