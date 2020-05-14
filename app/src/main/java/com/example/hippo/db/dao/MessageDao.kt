633b903819662a85444d94380f7fb9dd834a3d27
ivan tkachenko <me@ratijas.tk>
2020-05-14 14:09:52 +0300

    messages DAO

---
+++ app/src/main/java/com/example/hippo/db/dao/MessageDao.kt
@@ -0,0 +1,50 @@
+package com.example.hippo.db.dao
+
+import MessageId
+import UserId
+import androidx.room.*
+import com.example.hippo.db.entity.Message
+import io.reactivex.Completable
+import io.reactivex.Single
+import java.util.*
+
+@Dao
+interface MessageDao {
+    @Query("""
+        SELECT *
+        FROM messages
+        WHERE (sender = :peerId OR receiver = :peerId)
+        ORDER BY id ASC
+    """)
+    fun getChatHistory(peerId: UserId): Single<List<Message>>
+
+    @Query("""
+        SELECT *
+        FROM messages
+        WHERE (sender = :peerId OR receiver = :peerId)
+        ORDER BY id DESC
+        LIMIT 1
+    """)
+    fun getLastMessage(peerId: UserId): Single<Optional<Message>>
+
+    @Query("SELECT * FROM messages WHERE id = :mid")
+    fun getById(mid: MessageId): Single<Optional<Message>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(message: Message): Completable
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg messages: Message): Completable
+
+    @Delete
+    fun delete(message: Message): Completable
+
+    @Delete
+    fun deleteAll(vararg messages: Message): Completable
+
+    @Query("""
+        DELETE FROM messages
+        WHERE (sender = :peerId OR receiver = :peerId)
+    """)
+    fun deleteChatHistory(peerId: UserId): Completable
+}
